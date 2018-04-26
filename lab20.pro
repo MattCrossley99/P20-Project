263ebe8af524a40caeacfb8ff7372bfcb282414e
Matt <mattcrossley99@gmail.com>
2018-04-27 00:27:12 +0100

    First Commit

---
+++ lab20.pro
@@ -0,0 +1,26 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-02-04T13:43:54
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = lab20
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        window.cpp \
+    advancedScene.cpp \
+    sendWorker.cpp
+
+HEADERS  += window.h \
+    advancedScene.h \
+    sendWorker.h
+
+FORMS    += window.ui
+
+
---
+++ window.h
@@ -0,0 +1,43 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QPen>
+#include "advancedScene.h"
+
+namespace Ui {
+class Window;
+}
+
+class Window : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit Window(QWidget *parent = 0);
+    ~Window();
+
+signals:
+    void signalSendCanvasCleared();
+    
+private slots:
+    void on_actionQuit_triggered();
+
+    void sendWindow_mouseMoved(QPointF point);
+
+    void on_actionClear_triggered();
+
+private:
+    Ui::Window *ui;
+    advancedScene canvas_send;
+    QPen pen_send;
+    advancedScene canvas_receive;
+    QPen pen_receive;
+    qreal mousePrevX = 0;
+    qreal mousePrevY = 0;
+    qreal rcvPrevX = 0;
+    qreal rcvPrevY = 0;
+};
+
+#endif // WINDOW_H
